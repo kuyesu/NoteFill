fb7540940f4497b3bc0e90fde2a4617ee69e769b
kuyesu <kuyeso@halodevs.tech>
2022-08-01 00:35:15 +0300

    add google and apple login

--- components/auth/signIn.tsx
+++ components/auth/signIn.tsx
@@ -24,7 +24,10 @@ import {
     Typography
 
 } from '@mui/material';
-
+import clsx from 'clsx'
+import styles from './css/auth.module.css'
+import Google from '../icons/google.svg'
+import Apple from '../icons/apple.svg'
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -74,83 +77,100 @@ export default function Login() {
 
     return (
         <Card sx={style}>
-        <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 2, width: '55ch' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <div className="text justify-content-center">
-            <Typography 
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 2, },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                
+                    <Typography
                         //   variant="p" color="primary" 
-                        sx={{color: "#e2642c", paddingTop: 2, paddingLeft: 4, paddingBottom: 5,fontWeight: 100, fontSize: 18,  }}>
-                        LOGIN TO YOU ACCOUNT
+                        sx={{ color: "#e2642c", paddingTop: 2,fontWeight: 100, fontSize: 18, }}>
+                        Continue with 
                     </Typography >
-            </div>
-            <Form style={{ alignItems: "center" }}>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <TextField label="Email" color="warning" sx={{ m: 0, width: '35ch' }} />
-                </Form.Group>
-
-
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <FormControl sx={{ m: 0, width: '35ch', borderColor: "#e2642c" }} color="warning" variant="outlined"  >
-                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            type={values.showPassword ? 'text' : 'password'}
-                            value={values.password}
-                            onChange={handleChange('password')}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        onMouseDown={handleMouseDownPassword}
-                                        edge="end"
-                                    >
-                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            label="Password"
-                        />
-                    </FormControl>
-                </Form.Group>
-
-
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-
-                    <FormControlLabel
-                        control={
-                            <Switch checked={checked} onChange={handleChecked} name="check" style={{color: "#e2642c",}} />
-                            // style={{color: "#e2642c"}}
-                        }
-                        label="Keep me signed in"
-                        
-                    // inputProps={{ 'aria-label': 'controlled' }}
-                    />
-                </Form.Group>
-                <Stack
-                sx={{marginTop: 8}}
-                    direction="row">
-                    <Typography gutterBottom
-                        //   variant="p" color="primary" 
-                        component="div" sx={{ paddingTop: 1, fontWeight: 500, fontSize: 14, textAlign: "center", paddingLeft: 0, justifyContent: "left", marginLeft: 0 }}>
-                        <a href='/signup' style={{color: "#e2642c"}}>Create account</a>
-                    </Typography >
-                    <Button endIcon={<SendIcon />}
-                        //   variant="secondary" 
-                        type="submit"
-                        sx={{ backgroundColor: "#e2642c", color: "#fff", justifyContent: "right", marginLeft: 16 }}>
-                        Submit
-                    </Button>
+                    <Stack
+                    direction="row"
                     
-                </Stack>
-            </Form>
-        </Box>
+                    >
+                        <Button startIcon={<Google />}
+                            //   variant="secondary" 
+                            type="submit"
+                            className={clsx("", styles.button)} sx={{marginRight: 2}}>
+                            Google
+                        </Button>
+
+                        <Button startIcon={<Apple />}
+                            //   variant="secondary" 
+                            type="submit"
+                            className={clsx("", styles.button)}>
+                            Apple
+                        </Button>
+                    </Stack >
+                    <Form style={{ alignItems: "center" }}>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <TextField label="Email" color="warning" sx={{ m: 0, width: '35ch' }} />
+                        </Form.Group>
+
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <FormControl sx={{ m: 0, width: '35ch', borderColor: "#e2642c" }} color="warning" variant="outlined"  >
+                                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-password"
+                                    type={values.showPassword ? 'text' : 'password'}
+                                    value={values.password}
+                                    onChange={handleChange('password')}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                edge="end"
+                                            >
+                                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                    label="Password"
+                                />
+                            </FormControl>
+                        </Form.Group>
+
+
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+
+                            <FormControlLabel
+                                control={
+                                    <Switch checked={checked} onChange={handleChecked} name="check" style={{ color: "#e2642c", }} />
+                                    // style={{color: "#e2642c"}}
+                                }
+                                label="Keep me signed in"
+
+                            // inputProps={{ 'aria-label': 'controlled' }}
+                            />
+                        </Form.Group>
+                        <Stack
+                            sx={{ marginTop: 8 }}
+                            direction="row">
+                            <Typography gutterBottom
+                                //   variant="p" color="primary" 
+                                component="div" sx={{ paddingTop: 1, fontWeight: 500, fontSize: 14, }}>
+                                <a href='/signup' style={{ color: "#e2642c" }}>Create account</a>
+                            </Typography >
+                            <Button endIcon={<SendIcon />}
+                                //   variant="secondary" 
+                                type="submit"
+                                sx={{ backgroundColor: "#e2642c", color: "#fff", justifyContent: "right", marginLeft: 11 }}>
+                                Submit
+                            </Button>
+
+                        </Stack>
+                    </Form>
+            </Box>
         </Card>
     );
 }
