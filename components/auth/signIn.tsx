d12a8f19e17d6f806d5de508d1112120f7cbbe05
kuyesu <kuyeso@halodevs.tech>
2022-07-31 20:55:02 +0300

    add the login modal

---
+++ components/auth/signIn.tsx
@@ -0,0 +1,146 @@
+import * as React from 'react';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Form from 'react-bootstrap/Form';
+import SendIcon from '@mui/icons-material/AccountCircle';
+import {
+    Card,
+    Stack,
+    Switch,
+    TextField,
+    FormControl,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    FormHelperText,
+    IconButton,
+    Button,
+    FormControlLabel,
+    FormGroup,
+    FormLabel,
+    Box,
+    FilledInput,
+    Input,
+    Typography
+
+} from '@mui/material';
+
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    p: 4,
+    borderRadius: "8px",
+    boxShadow: "0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12)",
+};
+
+export default function Login() {
+
+
+
+    const [values, setValues] = React.useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+
+    const handleChange = (prop: string) => (event: { target: { value: any; }; }) => {
+        setValues({ ...values, [prop]: event.target.value });
+
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
+    };
+
+    const handleMouseDownPassword = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+    };
+
+    const handleChecked = (event: { target: { checked: boolean | ((prevState: boolean) => boolean); }; }) => {
+        setChecked(event.target.checked);
+    };
+    const [checked, setChecked] = React.useState(true);
+
+    return (
+        <Card sx={style}>
+        <Box
+            component="form"
+            sx={{
+                '& > :not(style)': { m: 2, width: '55ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <Form style={{ alignItems: "center" }}>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <TextField label="Email" color="secondary" sx={{ m: 0, width: '35ch' }} />
+                </Form.Group>
+
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <FormControl sx={{ m: 0, width: '35ch' }} variant="outlined" color="secondary" >
+                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
+                </Form.Group>
+
+
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+
+                    <FormControlLabel
+                        control={
+                            <Switch checked={checked} onChange={handleChecked} name="check" style={{color: "#e2642c"}} />
+                            // style={{color: "#e2642c"}}
+                        }
+                        label="Keep me signed in"
+                        
+                    // inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                </Form.Group>
+                <Stack
+                    direction="row">
+                    <Typography gutterBottom
+                        //   variant="p" color="primary" 
+                        component="div" sx={{ paddingTop: 1, fontWeight: 500, fontSize: 14, textAlign: "center", paddingLeft: 0, justifyContent: "left", marginLeft: 0 }}>
+                        <a href='/signup' style={{color: "#e2642c"}}>Create account</a>
+                    </Typography >
+                    <Button endIcon={<SendIcon />}
+                        //   variant="secondary" 
+                        type="submit"
+                        sx={{ backgroundColor: "#e2642c", color: "#fff", justifyContent: "right", marginLeft: 16 }}>
+                        Submit
+                    </Button>
+                </Stack>
+            </Form>
+        </Box>
+        </Card>
+    );
+}
--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -1,8 +1,30 @@
 import React, { Fragment } from 'react'
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 import styles from './css/navbar.module.css'
 import clsx from 'clsx'
+import Login from '../auth/signIn'
 
+
+// modal settings
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 function NavBar() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
     return (
         <Fragment>
             <nav className="navbar navbar-expand-lg navbar-light bg-white mb-5">
@@ -24,11 +46,11 @@ function NavBar() {
                     <div className="collapse navbar-collapse justify-content-end m-5" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link" href="#" style={{fontSize: 16, marginRight: 10}}>
+                                <a className="nav-link" href="#" style={{ fontSize: 16, marginRight: 10 }}>
                                     Features
                                 </a>
                             </li>
-                            <li className="nav-item" style={{fontSize: 16, marginRight: 10}}>
+                            <li className="nav-item" style={{ fontSize: 16, marginRight: 10 }}>
                                 <a className="nav-link" href="#">
                                     Pricing
                                 </a>
@@ -36,10 +58,25 @@ function NavBar() {
                             <li className="nav-item">
 
                                 <div className={clsx("pricing-button pt-10", styles.pricinbutton)}>
-                                    <button type="button" className={clsx("btn ", styles.button)} style={{
-                                        backgroundColor: "#e2642c",
-                                        color: "#fff",
-                                    }}>Sign In</button>
+                                    <button
+                                        onClick={handleOpen}
+                                        type="button"
+                                        className={clsx("btn ", styles.button)} style={{
+                                            backgroundColor: "#e2642c",
+                                            color: "#fff",
+                                        }}>Sign In</button>
+                                    <Modal
+                                        keepMounted
+                                        open={open}
+                                        onClose={handleClose}
+                                        aria-labelledby="keep-mounted-modal-title"
+                                        aria-describedby="keep-mounted-modal-description"
+                                    >
+                                        
+                                            <Login />
+                                       
+
+                                    </Modal>
                                 </div>
                             </li>
                         </ul>
