278a34456530bec6e5e1bda694ea5bc96588f922
Adiza <kuburatuadiza@gmail.com>
2023-01-22 15:18:10 +0100

    Added a logo on the nav bar

--- components/home/hero/index.tsx
+++ components/home/hero/index.tsx
@@ -3,7 +3,13 @@ import React from "react";
 export default function index() {
   return (
     <div className="py-4 px-48 mx-4 flex justify-between w-full z-50">
-      <div>left</div>
+      <div>
+      <a href="https://flowbite.com/" className="flex items-center">
+  <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+  <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+</a>
+
+      </div>
       <div>
         <button
           type="button"
