7a08f3964fff7d6ad9c871585451b13bfb99d191
kuyesu <2019bce028@std.must.ac.ug>
2023-01-22 08:12:56 +0300

    update to fresh dependencies

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
-
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-
-    return config;
-  }
-};
--- stripe/createCheckoutSession.ts
+++
@@ -1,8 +0,0 @@
-import { collection, addDoc } from "firebase/firestore";
-import { auth, db } from "../firebase/firebaseClient";
-import getStripe from "./initializeStripe"
-
-export async function createCheckoutSession(uid: string) {
-    const stripe = await getStripe()
-    
-}
