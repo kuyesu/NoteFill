e68cd144a1480300006ab8656b72cccc1d56bdba
Adiza <kuburatuadiza@gmail.com>
2023-01-23 15:16:24 +0100

    Changed the image

--- components/home/hero/index.tsx
+++ components/home/hero/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
+import Image from "next/image";
 
 export default function index() {
   return (
     <div className="py-4 px-48 mx-4 flex justify-between w-full z-50">
-      <div>
-      <a href="https://flowbite.com/" className="flex items-center">
-  <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-  <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-</a>
-
+      <div> 
+  <Image src="/images/logo.png" width={100} height={100} alt=""  className="h-16 mr-3"/>
       </div>
       <div>
         <button
